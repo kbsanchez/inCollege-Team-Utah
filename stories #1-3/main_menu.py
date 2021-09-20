9214604dbe2166bcfb2b7709289f9d078ad7699c
Jose Ramirez <proyectoramirez07@gmail.com>
2021-09-19 21:16:52 -0400

    Add options to main menu

--- stories #1-3/main_menu.py
+++ stories #1-3/main_menu.py
@@ -1,2 +1,49 @@
-def main_menu():
+def learn_skills_menu():
     pass
+
+
+def logout():
+    pass
+
+
+optionsAndActions = [
+    ("Job/Internship Search", None),
+    ("Find Someone You Know", None),
+    ("Learn a New Skill", learn_skills_menu),
+    ("Log Out", logout)
+]
+
+def print_menu_options():
+        options = [
+            f"{i + 1} - {x[0]}" 
+            for i, x 
+            in enumerate(optionsAndActions)
+        ]
+
+        options_text = "\n".join(options)
+
+        print(options_text)
+
+
+def get_user_action_selection():
+    selection_text = input("Please make a choice from the menu: ")
+    selection = int(selection_text) - 1
+
+    return optionsAndActions[selection][1]
+
+
+def main_menu():
+    while True:
+        print("\n")
+        print_menu_options()
+        print("\n")
+        
+        try:
+            action = get_user_action_selection()
+            
+            if action is None:
+                print("Under Construction")
+            else:
+                action()
+        except:
+            print("Invalid selection")
