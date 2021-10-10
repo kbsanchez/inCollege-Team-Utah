46c75f31f43fef5c6b3574a6f3e1348e267e205f
Jose Ramirez <proyectoramirez07@gmail.com>
2021-10-10 10:07:31 -0400

    Create tests for profile menu

--- test/context.py
+++ test/context.py
@@ -7,3 +7,5 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 from src import main
 from src import main_menu
 from src import links_menu
+from src import profile_menu
+from src import db_session
