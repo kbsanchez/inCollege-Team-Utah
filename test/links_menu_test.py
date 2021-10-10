de04bb5f86c39a2c45541d944d35beaeeccc924e
Jose Ramirez <proyectoramirez07@gmail.com>
2021-10-10 07:24:12 -0400

    Fix tests for links menu

--- test/links_menu_test.py
+++ test/links_menu_test.py
@@ -1,81 +1,51 @@
 import pytest
-import sqlite3
-from typing import Union
-from test.context import main, main_menu
-from test.utils import populate_db
-
-SAMPLE_USERNAME: str = "User1"
-SAMPLE_PASSWORD: str = "$Aample123"
-
-SAMPLE_FIRSTNAME: str = "John"
-SAMPLE_LASTNAME: str = "Smith"
-SAMPLE_UNREGISTERED_FIRSTNAME: str = "Joe"
-SAMPLE_UNREGISTERED_LASTNAME: str = "Doe"
-
-SAMPLE_UNREGISTERED_USERNAME: str = "User2"
-SAMPLE_UNREGISTERED_PASSWORD: str = "$Ample123"
-SAMPLE_SHORT_PASSWORD: str = "$Amp1e"
-SAMPLE_LONG_PASSWORD: str = "$Amp1e long password"
-SAMPLE_NO_UPPER_PASSWORD: str = "$ample123"
-SAMPLE_NO_DIGIT_PASSWORD: str = "$Ampleabc"
-SAMPLE_NO_ALPHA_PASSWORD: str = "Sample123"
-
-mock_db: Union[sqlite3.Connection, None] = None
-test_db: Union[sqlite3.Connection, None] = None
-
-def test_CopyrightNoticeMenu(capsys) -> None:
-    populate_db(test_db, SAMPLE_USERNAME, SAMPLE_PASSWORD)
-    main.login_attempt(SAMPLE_USERNAME, SAMPLE_PASSWORD)
-
-    selection = CopyrightNoticeMenu().run
-    assert = "This is a copyright notice"
+from .context import links_menu
 
 
-def test_AboutMenu(capsys) -> None:
-    populate_db(test_db, SAMPLE_USERNAME, SAMPLE_PASSWORD)
-    main.login_attempt(SAMPLE_USERNAME, SAMPLE_PASSWORD)
+def test_CopyrightNoticeMenu() -> None:
+    menu = links_menu.CopyrightNoticeMenu()
+    assert menu.title == "Copyright Notice"
+    assert menu.subtitle == "This is a copyright notice"
 
-    selection = AboutMenu().run
-    assert = "This is the about section"
 
-def test_AccessibilityMenu(capsys) -> None:
-    populate_db(test_db, SAMPLE_USERNAME, SAMPLE_PASSWORD)
-    main.login_attempt(SAMPLE_USERNAME, SAMPLE_PASSWORD)
+def test_AboutMenu() -> None:
+    menu = links_menu.AboutMenu()
+    assert menu.title == "About"
+    assert menu.subtitle == "This is the about section"
 
-    selection = AccessibilityMenu().run
-    assert = "This is the accessibility menu"
 
-def test_UserAgreementMenu(capsys) -> None:
-    populate_db(test_db, SAMPLE_USERNAME, SAMPLE_PASSWORD)
-    main.login_attempt(SAMPLE_USERNAME, SAMPLE_PASSWORD)
+def test_AccessibilityMenu() -> None:
+    menu = links_menu.AccessibilityMenu()
 
-    selection = UserAgreementMenu().run
-    assert = "This is the user agreement menu"
+    assert menu.title == "Accessibility"
+    assert menu.subtitle == "This is the accessibility menu"
 
-def test_PrivacyPolicyMenyu(capsys) -> None:
-    populate_db(test_db, SAMPLE_USERNAME, SAMPLE_PASSWORD)
-    main.login_attempt(SAMPLE_USERNAME, SAMPLE_PASSWORD)
 
-    selection = PrivacyPolicyMenu().run
-    assert = "This is the privacy policy"
+def test_UserAgreementMenu() -> None:
+    menu = links_menu.UserAgreementMenu()
+    assert menu.title == "User Agreement"
+    assert menu.subtitle == "This is the user agreement menu"
 
-def test_CookiePolicyMenyu(capsys) -> None:
-    populate_db(test_db, SAMPLE_USERNAME, SAMPLE_PASSWORD)
-    main.login_attempt(SAMPLE_USERNAME, SAMPLE_PASSWORD)
 
-    selection = CookiePolicyMenu().run
-    assert = "This is our cookie policy"
+def test_PrivacyPolicyMenu() -> None:
+    menu = links_menu.PrivacyPolicyMenu()
+    assert menu.title == "Privacy Policy"
+    assert menu.subtitle == "This is the privacy policy"
 
-def test_CopyrightPolicyMenyu(capsys) -> None:
-    populate_db(test_db, SAMPLE_USERNAME, SAMPLE_PASSWORD)
-    main.login_attempt(SAMPLE_USERNAME, SAMPLE_PASSWORD)
 
-    selection = CopyrightPolicyMenu().run
-    assert = "This is our copyright policy"
+def test_CopyrightPolicyMenu() -> None:
+    menu = links_menu.CopyrightPolicyMenu()
+    assert menu.title == "Copyright Policy"
+    assert menu.subtitle == "This is our copyright policy"
 
-def test_BrandPolicyMenyu(capsys) -> None:
-    populate_db(test_db, SAMPLE_USERNAME, SAMPLE_PASSWORD)
-    main.login_attempt(SAMPLE_USERNAME, SAMPLE_PASSWORD)
 
-    selection = BrandPolicyMenu().run
-    assert = "This is our brand policy"
+def test_CookiePolicyMenu() -> None:
+    menu = links_menu.CookiePolicyMenu()
+    assert menu.title == "Cookie Policy"
+    assert menu.subtitle == "This is our cookie policy"
+
+
+def test_BrandPolicyMenu() -> None:
+    menu = links_menu.BrandPolicyMenu()
+    assert menu.title == "Brand Policy"
+    assert menu.subtitle == "This is our brand policy"
