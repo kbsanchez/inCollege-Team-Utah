e13e9b0b9ada3c527c8b82e30da8a9dfbc98e17b
kbsanchez <66031870+kbsanchez@users.noreply.github.com>
2021-10-13 12:39:38 -0400

    add send_request, decline_request, accept_request, read_friend_request, and my_friends_list functions
    my_friends_list still in progress

--- src/friends.py
+++ src/friends.py
@@ -1,6 +1,6 @@
 import sqlite3
 from .db_session import db
-from .main_menu import get_user
+from .main_menu import get_user, get_user_selection
 
 conn = db
 c = conn.cursor()
@@ -19,5 +19,51 @@ def friends_entry(userOne, userRequested, request):
     query = """INSERT INTO Friends(userOne, userRequested, request) VALUES (?,?,?)"""
     c.execute(query, data)
     conn.commit()
+
+
+#updates key in friends table to send a request to another user to become friends
+def send_request(username, requestedUser):
+    query = """UPDATE Friends SET request = 1 WHERE userOne = ? AND userRequested = ?"""
+    target = (username, requestedUser)
+    c.execute(query, target)
+    conn.commit()
+
+
+#updates key in friends table to decline a friend request
+def decline_request(username, requestedUser):
+    query = """UPDATE Friends SET request = 0 WHERE userOne = ? AND userRequested = ?"""
+    target = (username, requestedUser)
+    c.execute(query, target)
+    conn.commit()
+
+
+#updates key in friends table to accept a friend request
+def accept_request(username, requestedUser):
+    query = """UPDATE Friends SET request = 2 WHERE userOne = ? AND userRequested = ?"""
+    target = (username, requestedUser)
+    c.execute(query, target)
+    conn.commit()
+
+
+#reads data from friends table
+def read_friend_requests():
+    friendRequests = []
+    user = get_user()
+    query = """SELECT userOne, userRequested, request FROM Friends WHERE request = 1 AND userRequested = ?"""
+    for student in c.execute(query, user):
+        friendRequests.append(student)
+
+
+#in progress: populate friends list
+def my_friends_list():
+    friendsList = []
+    user = get_user()
+    cursor = db.cursor()
+    query = """SELECT userOne, userRequested, request FROM Friends WHERE request = 2"""
+    selectUserOneQuery = """SELECT userOne FROM Friends WHERE request = 2"""
+    selectUserTwoQuery = """SELECT userRequested FROM Friends WHERE request = 2"""
+    for row in c.execute(query):
+        return
+
 
 
